08a6f0ee2b5b0bdfd182b3e6b9a44dff240c48b5
JuanVarelaAmaro <juan_albertvm@hotmail.com>
2026-03-06 02:44:56 -0600

    feat: Agregue los documentos para la base de datos

---
+++ BackEnd/Models/Usuarios.js
@@ -0,0 +1,47 @@
+const mongoose = require ('mongoose');
+
+const UsuarioSchema = new mongoose.Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    fechaNacimiento: {
+        type: Date,
+        required: true
+    },
+    pais: {
+        type: String,
+        required: true
+    },
+    correo: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    rol: { 
+        type: String, 
+        default: 'cliente'
+    },
+    billetera: {
+        saldo: { 
+        type: Number, 
+        default: 0 
+    },
+    tieneAdeudo: { 
+        type: Boolean, 
+        default: false 
+    }
+  }
+
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
